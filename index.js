2bb2d32f7d8ac6332cd8a661bbeb84f61c3cc26a
vertefra <verte.fra@gmail.com>
2020-10-07 18:41:37 -0400

    alloed moves metod added

--- index.js
+++ index.js
@@ -54,12 +54,67 @@ class Board {
     }
   }
 
+  returnPiece(coord) {
+    return this.board[Board.getKey(coord)];
+  }
+
   removePiece(coord) {
     const key = Board.getKey(coord);
     if (this.board[key] === " ") {
       throw "void cell, nothing to remove";
     } else {
       this.board[key] = 0;
+    }
+  }
+
+  allowedMoves(coord) {
+    const key = Board.getKey(coord);
+    const cell = this.board[key];
+    const allowedMoves = [];
+    const x = coord[0];
+    const y = coord[1];
+    const move_1 = [];
+    const move_2 = [];
+
+    switch (cell) {
+      case " ":
+        throw "selected cell is void";
+
+      case 0:
+        throw "selected cell is empty";
+
+      case "W":
+        // black pieces move from top to bottom
+
+        move_1.push(x - 1, y - 1);
+        move_2.push(x + 1, y - 1);
+
+        // the two allowed moves are [x-1, y+1] and [x+1, y+1]
+
+        if (this.inBoard(move_1)) allowedMoves.push(move_1);
+        if (this.inBoard(move_2)) allowedMoves.push(move_2);
+        return allowedMoves;
+
+      case "B":
+        // black pieces move from top to bottom
+
+        move_1.push(x - 1, y + 1);
+        move_2.push(x + 1, y + 1);
+
+        // the two allowed moves are [x-1, y+1] and [x+1, y+1]
+
+        if (this.inBoard(move_1)) allowedMoves.push(move_1);
+        if (this.inBoard(move_2)) allowedMoves.push(move_2);
+        return allowedMoves;
+    }
+  }
+
+  inBoard(coord) {
+    console.log(this.x, coord[0]);
+    if (coord[0] > this.x || coord[1] > this.y) {
+      return false;
+    } else {
+      return true;
     }
   }
 
@@ -70,15 +125,14 @@ class Board {
   static getCoord(key) {
     return key.split("-");
   }
-
-  movePiece(coordFrom, coordTo) {}
 }
 
-const board = new Board(8);
+const board = new Board((x = 8));
 board.initBoard();
 board.renderBoard();
 board.setPiece("W", [5, 5]);
 board.setPiece("B", [1, 1]);
-board.setPiece("B", [8, 8]);
+board.setPiece("B", [4, 4]);
 board.removePiece([1, 1]);
+console.log(board.allowedMoves([5, 5]));
 board.renderBoard();
